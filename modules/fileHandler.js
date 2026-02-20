cad25627e73864110ab156c4bad6652c2ee9ca05
anurag-kkumar <anukumar95084@gmail.com>
2026-02-20 10:21:08 +0530

    add feature

--- modules/fileHandler.js
+++ modules/fileHandler.js
@@ -1,7 +1,69 @@
-[
-    {
-        "id":1,
-        "name":"anurag",
-        
+const fs=require("fs");
+
+exports.home=(req,res)=>{
+  const data = fs.readFileSync("employee.json", "utf-8");
+    const employees = JSON.parse(data);
+
+    let totalSalary = 0;
+    let totaltax=0;
+    employees.forEach(emp => {
+        emp.salary = Number(emp.salary);     
+        emp.tax = emp.salary * 0.10;          
+        emp.netSalary = emp.salary - emp.tax; 
+
+        totalSalary += emp.salary;
+        totaltax += emp.tax;
+
+    });
+
+    const totalEmployees = employees.length;
+
+    res.render("index", {
+        employees,
+        totalEmployees,
+        totalSalary,
+        totaltax
+    });
+};
+
+exports.employeesForm=(req,res)=>{
+    res.render('add');
+};
+
+exports.add=(req,res)=>{
+
+    const { name, department, salary } = req.body;
+
+    if (!name || salary <= 0) {
+        return res.send("Invalid Input");
     }
-]
+    
+    fs.readFile("employee.json", "utf-8", (err, data) => {
+
+        let employees = [];
+
+        if (!err) {
+            employees = JSON.parse(data);
+        }
+
+        const newEmployee = {
+            id: Math.random(),
+            name,
+            department,
+            salary
+        };
+
+        employees.push(newEmployee);
+
+        fs.writeFile("employee.json", JSON.stringify(employees, null, 2), (err) => {
+
+            if (err) {
+                return res.send("Error saving data");
+            }
+
+            res.redirect("/");
+        });
+
+    });
+}
+
--- server.js
+++ server.js
@@ -11,50 +11,13 @@ app.use(express.json());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/employee/add', (req, res) => {
-    res.render('add');
-});
+const{home,employeesForm,add}=require("./modules/fileHandler");
 
-app.post('/employee/add', (req, res) => {
-
-    const { name, department, salary } = req.body;
-
-    if (!name || salary <= 0) {
-        return res.send("Invalid Input");
-    }
-
-    fs.readFile(filePath, "utf-8", (err, data) => {
-
-        let employees = [];
-
-        if (!err) {
-            employees = JSON.parse(data);
-        }
-
-        const newEmployee = {
-            id: Date.now(),
-            name,
-            department,
-            salary
-        };
-
-        employees.push(newEmployee);
-
-        fs.writeFile(employee.json, JSON.stringify(employees, null, 2), (err) => {
-
-            if (err) {
-                return res.send("Error saving data");
-            }
-
-            res.redirect("/");
-        });
-
-    });
-});
+app.get("/",home);
+app.get("/employee/add",employeesForm);
+app.post("/employee/add",add);
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
